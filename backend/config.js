ff211ca7a919959c6c8fb49e6dc56d2c2935db64
aniruddhapande4 <aniruddhapande4@gmail.com>
2024-12-23 15:43:51 +0530

    First Commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = process.env.PORT || 5555;
+
+export const mongoDBURL = 'mongodb://localhost:27017/book-store';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
