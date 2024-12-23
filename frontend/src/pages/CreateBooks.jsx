de84da0c2be51a1688bdd61149fa3f0c8a663a14
aniruddhapande4 <139002282+aniruddhapande4@users.noreply.github.com>
2024-12-23 17:39:33 +0530

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post('https://book-store-mern-stack-72vw.onrender.com/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
